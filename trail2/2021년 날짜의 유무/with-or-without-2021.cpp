90cc668e13c9187a6b4215d289ad344979f481f4
Issacreverse <99563622+issacreverse@users.noreply.github.com>
2026-06-12 03:02:57 +0900

    [Wrong Answer] 2021년 날짜의 유무 (2ms, 0MB)

---
+++ trail2/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool Func(int M, int D)
+{
+    if(M>12)
+        return false;
+    if(D>31)
+        return false;
+    if(M == 2 || M == 4 || M==6 || M==9 || M == 11)
+    {
+        if(D>30)
+            return false;
+    }
+    return true;
+}
+int main() {
+    // Please write your code here.
+    int M, D;
+    cin >> M >> D;
+    if(Func(M,D))
+        cout << "Yes";
+    else
+        cout << "No";
+    return 0;
+}
